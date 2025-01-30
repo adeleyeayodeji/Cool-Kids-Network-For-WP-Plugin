6a4abe8c464089d62a1a995f4be5c87500d8c226
adeleyeayodeji <biodunhi@gmail.com>
2025-01-30 19:14:44 +0100

    Add user role display to character details template

--- app/templates/character-details.php
+++ app/templates/character-details.php
@@ -22,20 +22,26 @@ $user_id = get_current_user_id();
 $first_name = get_user_meta($user_id, 'first_name', true);
 $last_name = get_user_meta($user_id, 'last_name', true);
 $country = get_user_meta($user_id, 'country', true);
-
+//get user role
+$user_role = wp_get_current_user()->roles[0];
+//remove underscore from user role and capitalize first letter
+$user_role = ucfirst(str_replace('_', ' ', $user_role));
 ?>
 <div class="<?php echo COOL_KIDS_NETWORK_WP_PREFIX; ?>character-details">
 	<p>
-		<?php _e('First Name:', 'cool-kids-network-wp'); ?> <?php echo $first_name ?: __('Not set', 'cool-kids-network-wp'); ?>
+		<?php _e('First Name:', 'cool-kids-network-wp'); ?> <?php echo esc_html($first_name) ?: __('Not set', 'cool-kids-network-wp'); ?>
 	</p>
 	<p>
-		<?php _e('Last Name:', 'cool-kids-network-wp'); ?> <?php echo $last_name ?: __('Not set', 'cool-kids-network-wp'); ?>
+		<?php _e('Last Name:', 'cool-kids-network-wp'); ?> <?php echo esc_html($last_name) ?: __('Not set', 'cool-kids-network-wp'); ?>
 	</p>
 	<p>
-		<?php _e('Country:', 'cool-kids-network-wp'); ?> <?php echo $country ?: __('Not set', 'cool-kids-network-wp'); ?>
+		<?php _e('Country:', 'cool-kids-network-wp'); ?> <?php echo esc_html($country) ?: __('Not set', 'cool-kids-network-wp'); ?>
 	</p>
 	<p>
 		<?php _e('Email:', 'cool-kids-network-wp'); ?> <?php echo wp_get_current_user()->user_email ?: __('Not set', 'cool-kids-network-wp'); ?>
+	</p>
+	<p>
+		<?php _e('Role:', 'cool-kids-network-wp'); ?> <?php echo esc_html($user_role) ?: __('Not set', 'cool-kids-network-wp'); ?>
 	</p>
 </div>
 
